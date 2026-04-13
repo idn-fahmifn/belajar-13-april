4a2dada4a421e604d71ec528ee4e4780fe59c2cd
idn-fahmifn <fahmifn.idn@gmail.com>
2026-04-13 14:58:06 +0700

    1.5

--- app/Http/Controllers/AgeController.php
+++ app/Http/Controllers/AgeController.php
@@ -11,8 +11,16 @@ class AgeController extends Controller
         return view('umur.form');
     }
 
-     public function success()
+    public function success()
     {
         return view('umur.success');
+    }
+
+    // dihubungkan ke method post, wajib ada parameter untuk menerima data dari request
+    public function send(Request $request)
+    {
+        // agar data bisa dibaca di dalam scope ini.
+
+        return $request;
     }
 }
