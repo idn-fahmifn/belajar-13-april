e096260049de41396b38279a2001af5878f65829
idn-fahmifn <fahmifn.idn@gmail.com>
2026-04-13 11:48:17 +0700

    first

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Basic Routing untuk menampilkan output.
+Route::get('/hallo', function (){
+    return "Hallo Laravel, saya sedang belajar laravel";
+});
+
+// Routing dengan parameter
+Route::get('/mobil/{param}', function($mobil){
+    return "Ini adalah halaman detail mobil. Mobil yang kamu pilih adalah : " . $mobil;
+});
+
+Route::get('/motor/{nama?}', function($motor = 'honda'){
+    return view('motor', compact('motor'));
+});
